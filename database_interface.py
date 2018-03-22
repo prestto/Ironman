cd2be659b5e0050ae74fcf80032be4fc48af9f33
prestto <tom.preston377@gmail.com>
2018-03-22 18:25:17 +0100

    add table exists check

--- database_interface.py
+++ database_interface.py
@@ -4,29 +4,41 @@ conn = sql.connect("./database/ironbase.db")
 
 c = conn.cursor()
 
-# req = c.execute("""
-#                 create table link_page (
-#                 id integer primary key,
-#                 link NVARCHAR(200),
-#                 event_name nvarchar(100),
-#                 datetime text,
-#                 city NVARCHAR(100),
-#                 country NVARCHAR(100),
-#                 year_table int,
-#                 location_table NVARCHAR(100),
-#                 distance NVARCHAR(50),
-#                 format NVARCHAR(50)
-#                 );
-#                 """)
+# Check that links page exists, if not create it
+tb_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='link_page'"
 
-c.execute("""
-          select count(link)
-          from link_page
-          where link = :link
-          """,
-          {"link" : "http://eu.ironman.com/triathlon/coverage/detail.aspx?race=bariloche70.3&y=2018"})
+if not co.execute(tb_exists).fetchone():
+    req = c.execute("""
+                  create table link_page (
+                  id integer primary key,
+                  link NVARCHAR(200),
+                  event_name nvarchar(100),
+                  datetime text,
+                  city NVARCHAR(100),
+                  country NVARCHAR(100),
+                  year_table int,
+                  location_table NVARCHAR(100),
+                  distance NVARCHAR(50),
+                  format NVARCHAR(50)
+                  );
+                  """)
+    
+tb_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='results_page'"
+if not co.execute(tb_exists).fetchone():
+    req = c.execute("""
+                ADD RESULTS PAGE HERE
+              );
+              """)
+# c.execute("""
+#           select count(link)
+#           from link_page
+#           where link = :link
+#           """,
+#           {"link" : "http://eu.ironman.com/triathlon/coverage/detail.aspx?race=bariloche70.3&y=2018"})
 
 # c.execute("drop table link_page")
+# c.execute("drop table results_page")
+
 
 # c.execute("""insert into link_page ('link', 'year_link', 'location_link', 'year_table', 'location_table', 'distance', 'format') values
 #           ('http://eu.ironman.com/triathlon/coverage/detail.aspx?race=florida70.3&y=2008'
@@ -35,7 +47,9 @@ c.execute("""
 
 # c.execute("select * from link_page")
 
-print(c.fetchone()[0])
+# print(c.fetchone()[0])
 
 conn.commit()
 conn.close()
+
+
